2f36b7622bb5cd0f4fc5de055e9027dfdb7667ef
pedro.santos <pedroabs.dev42@gmail.com>
2020-08-30 20:11:39 -0300

    Error Handling

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,37 +1,40 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
-    public async execute({ name, email, password }: Request): Promise<User> {
-        const usersRepository = getRepository(User);
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
 
-        const checkUserExists = await usersRepository.findOne({
-            where: { email },
-        });
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
 
-        if (checkUserExists) {
-            throw new Error('Email address already used.');
-        }
-
-        const hashedPassword = await hash(password, 8);
-
-        const user = usersRepository.create({
-            name,
-            email,
-            password: hashedPassword,
-        });
-
-        await usersRepository.save(user);
-
-        return user;
+    if (checkUserExists) {
+      throw new AppError('Email address already used.');
     }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
 }
 
 export default CreateUserService;
