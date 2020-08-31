c46b985f406630886eace978bc3497f70df4dff8
pedro.santos <pedroabs.dev42@gmail.com>
2020-08-31 14:01:43 -0300

    serving static files

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-
+import uploadConfig from './config/upload';
 import routes from './routes/index';
+
 import AppError from './errors/AppError';
 
 import './database';
@@ -11,6 +12,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
