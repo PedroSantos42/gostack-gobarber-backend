47d1b88ece6d09d254cd91651aaffced1acdac0d
pedro.santos <pedroabs.dev42@gmail.com>
2020-10-11 12:52:27 -0300

    add Appointment tests

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,15 +1,17 @@
 import { uuid } from 'uuidv4';
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { isEqual } from 'date-fns';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
