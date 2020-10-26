27cf0e6005730cd3dd29ad5a0e20c7baeff75db5
pedro.santos <pedroabs.dev42@gmail.com>
2020-10-26 19:40:07 -0300

    saving tokens in the database

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
     const usersRepository = new UsersRepository();
-    const updateAvatar = new UpdateUserAvatarService(usersRepository);
+    const diskStorageProvider = new DiskStorageProvider();
+
+    const updateAvatar = new UpdateUserAvatarService(
+      usersRepository,
+      diskStorageProvider,
+    );
 
     const user = await updateAvatar.execute({
       user_id: request.user.id,
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,12 +3,7 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-import IMailProvider from './MailProvider/models/IMailProvider';
-// import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
-
-container.registerSingleton<IMailProvider>('MailProvider', DiskStorageProvider);
