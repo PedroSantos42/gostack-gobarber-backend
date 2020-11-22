68f98a401cd669ae19b756e4f6e3451260d4a058
pedro.santos <pedroabs.dev42@gmail.com>
2020-11-22 00:36:02 -0300

    add Routes validations with celebrate

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -10,15 +11,25 @@ const providersController = new ProvidersController();
 const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
+const providerIdMiddleware = celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  },
+});
+
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
 providersRouter.get(
   '/:provider_id/day-availability',
+  providerIdMiddleware,
   providerDayAvailabilityController.index,
 );
+
 providersRouter.get(
   '/:provider_id/month-availability',
+  providerIdMiddleware,
   providerMonthAvailabilityController.index,
 );
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+import { errors } from 'celebrate';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
@@ -17,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
