877fd5ea815435c220a928415bf784c2e05fcef0
pedro.santos <pedroabs.dev42@gmail.com>
2020-11-29 12:14:52 -0300

    Add Redis as Cache Provider

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -15,6 +17,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,6 +28,10 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('chave');
+
+    console.log(cacheData);
+
     const appointments = await this.appointmentsRepository.findAllAvailabilityInDayByProvider(
       {
         provider_id,
@@ -31,6 +40,8 @@ class ListProviderAppointmentsService {
         day,
       },
     );
+
+    await this.cacheProvider.save('chave', 'valor');
 
     return appointments;
   }
---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,29 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+import ICacheProvider from '../models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  public async recover(key: string): Promise<string> {
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return 'null';
+    }
+
+    const parsedData = JSON.parse(data);
+
+    return parsedData;
+  }
+
+  public async invalidate(key: string): Promise<void> {}
+}
