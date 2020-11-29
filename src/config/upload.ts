6252d6876860a10301afa56c33bb24efe20875f8
pedro.santos <pedroabs.dev42@gmail.com>
2020-11-29 01:25:27 -0300

    storage provider Amazon S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,54 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    // disk: {
+    //   storage: StorageEngine;
+    // };
+    aws: {
+      region: string;
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder,
-  storage: multer.diskStorage({
-    destination: path.resolve(tmpFolder),
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+  multer: {
+    storage: multer.diskStorage({
+      destination: path.resolve(tmpFolder),
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      region: 'us-east-1',
+      bucket: 'app-gobarber-42',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -2,17 +2,19 @@ import { Request, Response } from 'express';
 import { classToClass } from 'class-transformer';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+// import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+import S3StorageProvider from '@shared/container/providers/StorageProvider/implementations/S3StorageProvider';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
     const usersRepository = new UsersRepository();
-    const diskStorageProvider = new DiskStorageProvider();
+    // const diskStorageProvider = new DiskStorageProvider();
+    const s3StorageProvider = new S3StorageProvider();
 
     const updateAvatar = new UpdateUserAvatarService(
       usersRepository,
-      diskStorageProvider,
+      s3StorageProvider,
     );
 
     const user = await updateAvatar.execute({
