61b63a0607a8772878d356c571087858d37369a9
pedro.santos <pedroabs.dev42@gmail.com>
2021-01-09 16:45:21 -0300

    adjusts for deploy

--- src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
+++ src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -9,7 +9,7 @@ class NotificationsRepository implements INotificationsRepository {
   private ormRepository: MongoRepository<Notification>;
 
   constructor() {
-    this.ormRepository = getMongoRepository(Notification, 'mongo');
+    this.ormRepository = getMongoRepository(Notification);
   }
 
   public async create({
